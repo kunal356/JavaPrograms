14c913660c253e6888dadc863312a56a9c9127ef
kunal356 <91783071+kunal356@users.noreply.github.com>
2022-03-08 22:40:40 +0530

    8/3

---
+++ leetCodeProblems/longestValidParantheses32.java
@@ -0,0 +1,23 @@
+class Solution {
+  //problem no 32
+    public int longestValidParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        int max=0;
+        st.push(-1);
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                st.push(i);
+            }else{
+                st.pop();
+                if(st.isEmpty()){
+                    st.push(i);
+                }else{
+                    int len = i- st.peek();
+                    max = Math.max(max,len);
+                }
+            }
+        }
+        return max;
+    }
+}
