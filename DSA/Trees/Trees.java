04f817dbe7112436dcfc90a7a71284f38dc292a4
kunal356 <91783071+kunal356@users.noreply.github.com>
2022-03-17 17:46:07 +0530

    added iterative function

--- DSA/Trees/Trees.java
+++ DSA/Trees/Trees.java
@@ -53,6 +53,45 @@ class Main{
         inorder(root.right);
         
     }
+    
+//     preorder using iteration
+    public static void preorderIterative(Node root){
+        
+        if(root==null) return;
+        Stack<Node> s = new Stack<>();
+        s.push(root);
+
+        while(!s.isEmpty()){
+            Node node = s.pop();
+            System.out.print(node.data+" ");
+
+            if(node.right!=null){
+                s.push(node.right);
+            }if(node.left!=null){
+                s.push(node.left);
+            }
+        }
+    }
+
+    // inorder using iteration
+    public static void inorderIterative(Node root){
+        
+        if(root==null) return;
+        Stack<Node> s = new Stack<>();
+        Node curr = root;
+
+        while(curr!=null || s.size()>0){
+            while(curr!=null){
+                s.push(curr);
+                curr=curr.left;
+            }
+        curr=s.pop();
+        System.out.print(curr.data+" ");
+        curr = curr.right;
+        }
+        
+    }
+//postOrder Iterative remaining
 
 
 
