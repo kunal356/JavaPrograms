239f1a4b251381127b1a4e1c9e44ad22c92dc06b
kunal356 <91783071+kunal356@users.noreply.github.com>
2022-03-17 16:57:17 +0530

    roman to integer

---
+++ leetCodeProblems/RomanToInteger13.java
@@ -0,0 +1,28 @@
+package leetCodeProblems;
+import java.util.*;
+// 13 roman number to integer
+class RomanToInteger {
+    public static int romanToInt(String s) {
+        
+        HashMap<Character,Integer> m = new HashMap<>();
+        m.put('I',1);
+        m.put('V',5);
+        m.put('X',10);
+        m.put('L',50);
+        m.put('C',100);
+        m.put('D',500);
+        m.put('M',1000);
+        int result = m.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2;i>=0;i--){
+            if(m.get(s.charAt(i)) < m.get(s.charAt(i+1)) ) {
+            	result-=m.get(s.charAt(i));
+            }else {
+            	result+=m.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+		System.out.println(romanToInt("XIV"));
+	}
+}
