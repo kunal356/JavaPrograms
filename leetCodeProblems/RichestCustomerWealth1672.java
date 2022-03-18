37485be01814f69c64854d5b71673f78695af2e0
kunal356 <91783071+kunal356@users.noreply.github.com>
2022-03-18 22:25:56 +0530

    Array

---
+++ leetCodeProblems/RichestCustomerWealth1672.java
@@ -0,0 +1,16 @@
+// leet code problem 1672
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxSum = 0;
+        for(int i=0; i<accounts.length; i++){
+            int sum=0;
+            for(int j=0; j<accounts[i].length; j++){
+               sum+= accounts[i][j];
+            }
+            if(sum>maxSum){
+                maxSum = sum;
+            }
+        }
+        return maxSum;
+    }
+}
