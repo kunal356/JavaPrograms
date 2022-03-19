898c226e907ca1226620c0dca19df678b484290c
kunal356 <91783071+kunal356@users.noreply.github.com>
2022-03-19 09:15:31 +0530

    array questions

---
+++ leetCodeProblems/FinalValueOfVariableAfterPerformingOperations2011.java
@@ -0,0 +1,16 @@
+// leetcode problem 2011
+lass Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int val = 0;
+        for(int i=0; i< operations.length; i++){
+            if(operations[i].charAt(1) == '-'){
+                val--;
+            }else{
+                val++;
+            }
+           
+       
+    }
+        return val;
+    }
+}
